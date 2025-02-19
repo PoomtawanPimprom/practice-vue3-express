e18fd307b44f2933d0c8d565a381a3e70093e8b7
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-02-19 16:04:43 +0700

    update route at frontend

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,18 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import ApiView from "../views/ApiTestView.vue"
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'api',
+      component: ApiView,
     },
     {
-      path: '/todos',
-      name: 'todos',
-      component: () => import('../views/TodosView.vue'),
+      path: '/home',
+      name: 'home',
+      component: HomeView,
     },
     {
       path: '/about',
@@ -21,14 +21,6 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
-    },
-    {
-      path: '/api',
-      name: 'api',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/ApiTestView.vue'),
     },
   ],
 })
