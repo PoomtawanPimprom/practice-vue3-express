fd1ea61ff8d3680eb77aa773bae99ad8a6edf553
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-02-19 16:00:15 +0700

    add cors

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,18 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser')
 require('dotenv').config()
 const mysql = require('mysql2')
-
+const cors = require('cors')
 // Import Routes
 const HelloRoute = require('./router/hello/hello.route.js');
 const UserRoute = require('./router/user/user.route.js');
 
 const app = express();
 
+const corsOption = {
+    origin: 'http://localhost:5173',
+}
 
-
+app.use(cors(corsOption))
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 // Routes
