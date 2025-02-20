ecb8c5f84619cf3f0dd2ab02a3a608ee8ca53cfc
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-02-20 13:39:22 +0700

    add route path /slot

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ApiView from "../views/ApiTestView.vue"
+import SlotView from '@/views/SlotView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -15,6 +16,11 @@ const router = createRouter({
       component: HomeView,
     },
     {
+      path: '/slot',
+      name: 'slot',
+      component: SlotView,
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
@@ -22,6 +28,7 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+
   ],
 })
 
