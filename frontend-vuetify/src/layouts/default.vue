a01a31d0b72fb4a967033146401f8070f758828f
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-02-24 16:22:31 +0700

    add navbar

--- frontend-vuetify/src/layouts/default.vue
+++ frontend-vuetify/src/layouts/default.vue
@@ -1,9 +1,20 @@
 <template>
+  <v-app-bar>
+    <v-app-bar-title>
+      <v-btn to="/">logo</v-btn>
+      
+    </v-app-bar-title>
+    <v-spacer></v-spacer>
+    <v-btn to="/cart">cart</v-btn>
+    <v-btn to="/profile">profile</v-btn>
+    <v-btn >
+
+    </v-btn>
+  </v-app-bar>
   <v-main>
     <router-view />
   </v-main>
 
-  <AppFooter />
 </template>
 
 <script lang="ts" setup>
