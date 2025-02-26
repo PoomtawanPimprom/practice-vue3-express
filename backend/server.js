17ca8ce1b6673aae04c4e850aad48a86765f5979
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-02-26 08:17:00 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,12 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 require('dotenv').config()
-const mysql = require('mysql2')
 const cors = require('cors')
 // Import Routes
 const HelloRoute = require('./router/hello/hello.route.js');
 const UserRoute = require('./router/user/user.route.js');
-
+const RoleRoute = require('./router/role/role.route.js')
+const ProductRoute = require('./router/product/product.route.js')
 const app = express();
 
 const corsOption = {
@@ -21,6 +21,8 @@ app.use(bodyParser.json());
 // Routes
 app.use('/api/hello', HelloRoute);
 app.use('/api/user', UserRoute);
+app.use('/api/role', RoleRoute);
+app.use('/api/product', ProductRoute);
 // Start Server
 app.listen(8080, () => {
     console.log("Server started on port 8080");
