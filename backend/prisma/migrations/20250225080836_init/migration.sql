bb906f06eef012de468a4d3df56a9a524177ed7f
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-02-26 08:15:04 +0700

    implement prisma into backend

---
+++ backend/prisma/migrations/20250225080836_init/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `fname` VARCHAR(191) NOT NULL,
+    `lname` VARCHAR(191) NOT NULL,
+    `roldId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Role` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `roleName` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Product` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `price` INTEGER NOT NULL,
+    `quantity` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `User` ADD CONSTRAINT `User_roldId_fkey` FOREIGN KEY (`roldId`) REFERENCES `Role`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
